e268463b15361c7e1b526cee3567fbdb35d0b028
sgp124 <46280676+sgp124@users.noreply.github.com>
2021-11-12 12:21:54 -0500

    model file for form to take user input

---
+++ app/model/KeyTermData.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.List;
+
+public class KeyTermData {
+
+    public String keyTerm;
+
+	public String getKeyTerm() {
+		return keyTerm;
+	}
+
+	public void setKeyTerm(String keyTerm) {
+		this.keyTerm = keyTerm;
+	}
+    
+ }
