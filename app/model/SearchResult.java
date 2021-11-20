ac9daed418ef3ecd85ff6d853dde1b12699f917d
varshinichowdary27 <37653948+varshinichowdary27@users.noreply.github.com>
2021-11-20 14:14:01 -0500

    Main page search result extractor

---
+++ app/model/SearchResult.java
@@ -0,0 +1,26 @@
+package models;
+
+/**
+ * This class initializes all the attributes pertaining to Github
+ */
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class SearchResult {
+  public String name;
+  public String full_name;
+  public ArrayList<String> topics;
+  public Hashtable<String, String> owner = new Hashtable<String, String>();
+
+  /*public String body;
+  public String subreddit;*/
+
+  public SearchResult() {}
+  public SearchResult(String name,String full_name, ArrayList<String> topics, Hashtable<String, String> owner) {
+    this.name = name;
+    this.full_name = full_name;
+    this.topics = topics;
+	this.owner = owner;
+  }
+}
